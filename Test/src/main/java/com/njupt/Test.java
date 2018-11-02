146723c627960ddf18736bcdf8a439ff756a62fd
NeDrH <544012324@qq.com>
2018-11-02 11:34:13 +0800

    test

--- Test/src/main/java/com/njupt/Test.java
+++ Test/src/main/java/com/njupt/Test.java
@@ -2,6 +2,13 @@ package com.njupt;
 
 public class Test {
     public static void main(String[] args){
+        String str = createStr();
         System.out.println();
     }
+
+    private static String createStr() {
+        return "test";
+    }
+
+
 }
